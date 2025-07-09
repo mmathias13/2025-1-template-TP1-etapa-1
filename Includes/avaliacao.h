25a05a58754818fe0fa8845e64b0206b2fa2f118
blegora <bruno.leg@gmail.com>
2025-07-09 18:51:45 -0300

    Template - v0 com comentários nos .h

--- Includes/avaliacao.h
+++ Includes/avaliacao.h
@@ -3,8 +3,23 @@
 
 typedef struct Avaliacao tAvaliacao;
 
+
+/// @brief Função que cria um objeto do tipo tAvaliação*
+/// @param cpf - CPF da pessoa que fez a avaliação
+/// @param nota - A nota que a pessoa deu
+/// @return O objeto avaliação criado
 tAvaliacao* criaAvaliacao(char* cpf, int nota);
+
+
+/// @brief Retorna a nota de uma avaliação
+/// @param a 
+/// @return A nota de uma avaliação
 int getNotaAvaliacao(tAvaliacao *a);
+
+
+/// @brief Retorna o CPF de quem fez aquela avaliação
+/// @param a 
+/// @return O CPF do responsável pela avaliação
 char* getCpfAvaliacao(tAvaliacao *a);
 
 
--- Includes/util.h
+++ Includes/util.h
@@ -14,8 +14,15 @@ typedef enum
 } TipoAssinatura;
 
 
+/// @brief Função que converte uma quantidade de minutos em horas e minutos
+/// @param tempoMinutos Um inteiro. Ex: 280 
+/// @param horas A quantidade inteira de horas que cabem no tempoMinutos. Ex: Para 280, cabem 6 horas
+/// @param minutos A quantidade inteira de minutos que restaram após subtrair as horas inteiras. Ex: 20 
 void converteMinutos(int tempoMinutos, int *horas, int *minutos);
 
+/// @brief Função que, dada uma sigla de idioma, retorna o significado dela
+/// @param idioma - Sigla L/D/DL
+/// @return Uma string "Legendado", para entrada L, "Dublado", para entrada D, e "Dublado/Legendado" para entrada DL
 const char *descricaoIdioma(const char idioma[3]);
 
 #endif
